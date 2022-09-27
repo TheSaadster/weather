1d5ecca9888566f12d71b23b33abc57d58d0061e
Saad Aulakh <saadaulakh04@gmail.com>
2022-09-26 20:59:00 -0400

    Finished JavaScript

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-async function getWeather(){
-    //const response = await fetch('api.openweathermap.org/data/2.5/weather?q=London,uk&appid=6425b7df947268c09e6e0e80a94bff21', {mode: 'cors'});
-    //const weatherData = await response.json();
-    //console.log(weatherData);
-    var key = '6425b7df947268c09e6e0e80a94bff21';
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=' + key)  
-    .then(function(resp) { return resp.json() }) // Convert data to json
-    .then(function(data) {
-        console.log(data);
-    })
-    .catch(function() {
-        // catch any errors
-    });
+
+let weather = {
+    "apiKey": "6425b7df947268c09e6e0e80a94bff21",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=imperial&appid="
+            + this.apiKey)
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity, feels_like } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " mph";
+        document.querySelector(".feels-like").innerText = "Feels Like: " + feels_like + "°F";
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/random/1920x1080/?"+ name + ", landscape')"
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
 }
 
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+})
 
-getWeather();
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})
+
+weather.fetchWeather("Philadelphia");
